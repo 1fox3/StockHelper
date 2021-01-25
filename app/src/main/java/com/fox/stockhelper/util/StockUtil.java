1747c2da7399d093e0f8b1431e14033de5d2fab4
lusongsong2 <lusongsong@jd.com>
2021-01-25 16:50:53 +0800

    删除一批无用文件

--- app/src/main/java/com/fox/stockhelper/util/StockUtil.java
+++ app/src/main/java/com/fox/stockhelper/util/StockUtil.java
@@ -1,20 +1,11 @@
 package com.fox.stockhelper.util;
 
-import com.fox.stockhelper.api.stock.stockmarket.LastDealDateApi;
-
 /**
  * 股市工具类
+ *
  * @author lusongsong
  * @date 2020/8/20 15:06
  */
 public class StockUtil {
 
-    /**
-     * 最新交易日
-     * @return
-     */
-    public static String lastDealDate() {
-        LastDealDateApi lastDealDateApi = new LastDealDateApi();
-        return (String) lastDealDateApi.request();
-    }
 }
